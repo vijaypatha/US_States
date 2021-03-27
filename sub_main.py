df71b6f3808aae422a345e501f5f06efcfdf56b9
vijay <elderpatha@gmail.com>
2021-03-27 15:58:35 -0700

    accessing a cell

--- sub_main.py
+++ sub_main.py
@@ -4,8 +4,12 @@ df = pd.read_csv("50_states.csv")
 print(df.head())
 # Access the series, row, cell
 
+# Accessing a column
 # print(df.state)
 
+# Accessing a row
 state = df[df.state == 'Arizona']
+
+#Accessing a value of a row
 print(int(state.x), int(state.y))
 
